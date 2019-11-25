88f87745b862ab1c413f7388f3d93e544c8a05f6
mingge <598885353@qq.com>
2019-11-25 13:49:00 +0800

    提交springboot_git项目

---
+++ src/test/java/com/hw/SpringGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
